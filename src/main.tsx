06afd5423152d5fa63a8f046434b9da630752d24
nuno-oliveira <nuno.oliveira@builtcolab.pt>
2025-09-02 21:33:58 +0100

    font loading optimization

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+const loadFonts = () => {
+  document.documentElement.classList.add('fonts-loaded');
+  document.fonts.ready.then(() => {
+    document.body.style.fontFamily = 'Work Sans, sans-serif';
+    document.body.offsetHeight;
+  });
+};
+
+loadFonts();
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
